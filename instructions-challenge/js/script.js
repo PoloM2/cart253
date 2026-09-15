e3f11216712e8608ef5f87fb0f2b4bef6011de15
benedict-cart2025 <anastasovskimarko@gmail.com>
2026-09-15 16:00:33 -0400

    started canvas
    sunset and ground

--- instructions-challenge/js/script.js
+++ instructions-challenge/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
@@ -20,5 +20,22 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background("orange");
+    fill(255,200,110);
+    stroke(255,213,128)
+    ellipse(320,500,345);
+    fill("lightyellow");
+    stroke("red")
+    ellipse(320,500,280);
+    fill("darkgreen")
+    stroke("green")
+    rect(0, 475, 640, 300)
+    fill ("grey")
+    stroke("black")
+    triangle(0,475,100,475,50,200)
 
+    // background(640,600, 480)
+    
+
+    // Write stuff here that makes a landscape
 }
