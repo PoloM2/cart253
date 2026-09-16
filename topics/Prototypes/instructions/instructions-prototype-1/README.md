4b822353f969b7465d31ba456bad26efc348a0dd
PoloM2 <anastasovskimarko@gmail.com>
2026-09-16 16:47:01 -0400

    Made star and ship base and started sails plus background
    LEARNED TO CHECK P5 REFERENCE TO UNDERSTAND SHAPES AND WHAT ALL THE PARAMETERS MEAN

--- topics/Prototypes/instructions/instructions-prototype-1/README.md
+++ topics/Prototypes/instructions/instructions-prototype-1/README.md
@@ -1,6 +1,6 @@
-# TITLE OF PROJECT
+# Star Cruse
 
-AUTHOR NAME
+Marko Anastasovski
 
 [View this project online](URL_FOR_THE_RUNNING_PROJECT)
 
--- topics/Prototypes/instructions/instructions-prototype-1/js/script.js
+++ topics/Prototypes/instructions/instructions-prototype-1/js/script.js
@@ -1,9 +1,9 @@
 /**
- * Title of Project
- * Author Name
+ * Stardust Cruse
+ * Marko Anastasovski
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * A ship crusing through stars ?
+ *
  */
 
 "use strict";
@@ -12,13 +12,33 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(640, 640);
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * A ship a
 */
 function draw() {
+    background("purple");
+    fill("Brown");
+    stroke("black");
+    strokeWeight(20);
+    arc(500, 580, 345,300,0,280, OPEN);
+    //star piece 1
+    fill ("yellow")
+    stroke("gold")
+    strokeWeight(10)
+    triangle(15,275,115,275,60,350)
+    //second star piece
+    fill ("yellow")
+    stroke("gold")
+    strokeWeight(10)
+    triangle(15,320,115,320,60,240)
+    //ship sails LEARNED TO CHECK P5 REFERENCE TO UNDERSTAND SHAPES AND WHAT ALL THE PARAMETERS MEAN
+    rect(600, 400, 20, 100)
+    
+    
 
 }
