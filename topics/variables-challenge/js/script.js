f2cbc082d5f22b03eb855a873102a04d6282625e
PoloM2 <anastasovskimarko@gmail.com>
2026-09-22 15:43:05 -0400

    added bird and day to night
    birdy came and pissed off mrfurious

--- topics/variables-challenge/js/script.js
+++ topics/variables-challenge/js/script.js
@@ -17,10 +17,16 @@ let mrFurious = {
   fill: {
     r: 255,
     g: 225,
-    b: 225
+    b: 225,
   }
 };
 
+//the sky is darkening, the clouds are gathering, and Mr. Furious is getting madder and madder.
+let sky = {
+    r : 160,
+    g : 180,
+    b : 200,
+}
 /**
  * Create the canvas
  */
@@ -28,11 +34,18 @@ function setup() {
   createCanvas(400, 400);
 }
 
+let bird = {
+    x : 500,
+    y : 150,
+    velocity : 1
+
+}
+
 /**
  * Draw (and update) Mr. Furious
  */
 function draw() {
-  background(160, 180, 200);
+  background(sky.r, sky.g, sky.b);
   
   // Draw Mr. Furious as a coloured circle
   push();
@@ -42,6 +55,18 @@ function draw() {
   pop();
   mrFurious.fill.g = mrFurious.fill.g - 1;
   mrFurious.fill.b = mrFurious.fill.b - 1;
+  sky.r = sky.r- 0.5;
+  sky.g = sky.g- 0.5;
+  sky.b = sky.b- 0.5;
+
+  push()
+  fill("yellow")
+  stroke("orange");
+  square(bird.x, bird.y, 25);
+  pop()
+  bird.x = bird.x * 0.991;
+
+
 
 }
 
