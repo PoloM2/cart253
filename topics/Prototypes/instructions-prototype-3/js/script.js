9edbc3ff69678ef8c433b83b41b5e4edb76795ae
PoloM2 <anastasovskimarko@gmail.com>
2026-09-22 21:02:58 -0400

    finished 3rd prototype
    used what i learned from variable and made bouncing blue blur with dramatic effect using change of background color with rect()

--- topics/Prototypes/instructions-prototype-3/js/script.js
+++ topics/Prototypes/instructions-prototype-3/js/script.js
@@ -7,35 +7,57 @@
 
 "use strict";
 
-/**
- * Green rect for Background, Blue cirle for hedghog, Black lines for spinning background effect
-*/
-function setup() {
-    
-createCanvas(1000, 1000, WEBGL);
-
-}
-
-let beam ={
-    x: 1000,
-    y: 100,
-    size: 25,
-    fill: "black",
+// The blue blur charges his attack
+let ball = {
+  // Position and size
+  x: 200,
+  y: 200,
+  size: 200,
+  // Colour
+  fill: {
+    r: 0,
+    g: 0,
+    b: 225,
+  }
 };
 
+//the sky is darkening, the clouds are gathering, and Mr. Furious is getting madder and madder.
+let bG = {
+    r : 10,
+    g : 10,
+    b : 250,
+}
 /**
- * It will be a spinning blue circle, and a spining background made with rectangle and spining lines.
-*/
-function draw() {
+ * 
+ */
+function setup() {
+  createCanvas(400, 400);
+}
 
-    background("green");
-    fill("black");
-    stroke("black");
-    line(-500,0,500,0);
-    //spinning background effect
-    fill("blue");
-    stroke("rgb(7, 130, 245)");
-    strokeWeight(10);
-    circle(0, 0, 300);
+/**
+ * Draw (and update) Mr. Furious
+ */
+function draw() {
+  background(bG.r, bG.g, bG.b);
+  push();
+  noStroke();
+  fill(ball.fill.r, ball.fill.g, ball.fill.b);
+  
+
+  ellipse(random(-50, 50)+ random(-50, 50), ball.y, ball.size);
+  pop();
+  ball.fill.r = ball.fill.r - 1;
+  ball.fill.g = ball.fill.g - 1;
+
+//making an affect to showcase impact of charging attack
+  bG.g = bG.g- 2;
+  bG.b = bG.b- 2;
+  bG.r = bG.r+ 1;
+//grass
+fill("green");
+rect(0, 300, 400, 305);
+ 
+
+
 
 }
